3a4942e867bd5ef7f92cfd2c13613539be553830
jacky.leung <jacky.leung@oocl.com>
2018-11-29 14:20:07 +0800

    [LEUNGJA] feat: enable h2 database

--- src/main/java/com/tw/apistackbase/application/EmployeeRepository.java
+++ src/main/java/com/tw/apistackbase/application/EmployeeRepository.java
@@ -3,8 +3,6 @@ package com.tw.apistackbase.application;
 import com.tw.apistackbase.core.Employee;
 import org.springframework.data.repository.CrudRepository;
 
-import java.util.List;
-
 public interface EmployeeRepository extends CrudRepository<Employee, Long> {
 
 }
